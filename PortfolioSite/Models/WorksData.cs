137d0e7f04c55d37064755ddd8a55574242793bf
Jakobb32866 <jaszoch@gmail.com>
2026-04-27 21:13:46 +0200

    post fix

--- PortfolioSite/Models/WorksData.cs
+++ PortfolioSite/Models/WorksData.cs
@@ -7,6 +7,6 @@ public static class WorksData
         new("ta-strona", "project", "2026-04",
             "Ta strona", "This website",
             ["Blazor", "C#", ".NET", "HTML", "CSS", "Github", "Git", "Rider", "JavaScript"],
-            "https://github.com/jaszoch/portfolio", "https://github.com/Jakobb32866/jakobb32866.github.io")
+            "https://github.com/Jakobb32866/jakobb32866.github.io", "https://jakobb32866.github.io")
     ];
 }
